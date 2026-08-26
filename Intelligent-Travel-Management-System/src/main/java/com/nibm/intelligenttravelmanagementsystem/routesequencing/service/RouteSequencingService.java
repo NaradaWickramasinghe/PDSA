dd8a942c1cf0b84058f6d14459ff901d71d599d2
Zekennn <senurajr2005@gmail.com>
2026-08-26 20:20:22 +0530

    refactor(module-4): simplify Group 4 package structure, configure Supabase database, and add Postman API collection

--- Intelligent-Travel-Management-System/src/main/java/com/nibm/intelligenttravelmanagementsystem/routesequencing/service/RouteSequencingService.java
+++
@@ -1,9 +0,0 @@
-package com.nibm.intelligenttravelmanagementsystem.routesequencing.service;
-
-import com.nibm.intelligenttravelmanagementsystem.routesequencing.dto.ItineraryPlanRequest;
-import com.nibm.intelligenttravelmanagementsystem.routesequencing.dto.ItineraryPlanResponse;
-
-public interface RouteSequencingService {
-
-    ItineraryPlanResponse createPersonalizedItinerary(ItineraryPlanRequest request);
-}
